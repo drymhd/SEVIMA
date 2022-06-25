fbadaf72f24fe342197106651abe427f83d013e5
DRYMHD <darmah250903@gmail.com>
2022-06-25 09:00:09 +0700

    pembuatan migration model yang diperlukan

---
+++ app/Models/GuruDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GuruDetail extends Model
+{
+    use HasFactory;
+}
